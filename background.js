10aca1220a6be6d8735362bc78fbb57f55061a43
Gary Henderson <hendergy@gmail.com>
2020-07-23 17:41:07 -0400

    added getDocumentIds and getDocument functions to background, updated manifest permissions

--- background.js
+++ background.js
@@ -1,6 +1,29 @@
-let commandTest = () => {
-  console.log("Command test");
-};
+const DOCUMENT_FETCH_URL = "https://www.dotloop.com/my/rest/v1_0/folder";
+
+async function getDocumentIds(folderId) {
+  let response = await fetch(
+    `https://www.dotloop.com/my/rest/v1_0/folder/${folderId}/document?batchNumber=1&batchSize=20&_=1595536501587`
+  );
+
+  let documents = await response.json();
+  console.log(documents);
+
+  let documentIds = documents.map((d) => d.documentId).filter((d) => d);
+  console.log(documentIds);
+
+  return documentIds;
+}
+
+async function getDocumentData(documentId) {
+  let response = await fetch(
+    `https://www.dotloop.com/my/rest/v1_0/document/${documentId}/revision/0?_=1595539388679`
+  );
+
+  let document = await response.json();
+  console.log(document);
+
+  return document;
+}
 
 chrome.runtime.onInstalled.addListener(function () {
   // extension icon is active when the url matches hostEquals
@@ -17,20 +40,9 @@ chrome.runtime.onInstalled.addListener(function () {
     ]);
   });
 
-  chrome.runtime.onMessage.addListener(function (request, sender, response) {
-    console.log(
-      sender.tab
-        ? "from a content script: " + sender.tab.url
-        : "from an extentions"
-    );
-    if (request.greeting == "Hello world!") {
-      sendResponse({ farewell: "Goodbye!" });
-    }
-  });
-
   // allows user to press hotkey to run extension command
   chrome.commands.onCommand.addListener(function (command) {
     console.log("Hotkey command: " + command);
-    commandTest();
+    getDocumentIds(23923629);
   });
 });
--- contentscript.js
+++ contentscript.js
@@ -1,3 +1,1 @@
-chrome.runtime.sendMessage({ greeting: "Hello world!" }, function (response) {
-  console.log(response.farewell);
-});
+
