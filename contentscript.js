0a2bed64887cccddb45e38fc48f4f226b406b51c
Gary Henderson <hendergy@gmail.com>
2020-08-05 18:26:59 -0400

    created getFolderId function, add chrome messaging to pass folderId to background file

--- contentscript.js
+++ contentscript.js
@@ -1,1 +1,18 @@
+const getFolderId = () => {
+  let base_url = "https://www.dotloop.com/my/templates#folder/";
+  let url = window.location.href;
+  // https://www.dotloop.com/my/templates#folder/23923629
+  let folderId = url.replace(base_url, "");
 
+  return folderId;
+};
+
+document.querySelectorAll("li.section-item").forEach((folder) => {
+  folder.addEventListener("click", getFolderId);
+});
+
+let folderId = getFolderId();
+
+console.log("The folderId is " + folderId);
+
+chrome.runtime.sendMessage(folderId);
